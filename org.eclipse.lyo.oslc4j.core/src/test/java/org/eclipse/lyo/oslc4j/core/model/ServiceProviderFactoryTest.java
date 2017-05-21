4c1cc4df757b81a41d3b23c16fd42892eb3183d0
Jad <jad@kth.se>
2017-05-22 00:02:26 +0200

    Bug 430390 - Missing call to resolve path parameters in method-path annotation for CreationFactory annotation
    resolving path parameters in @path annotations of both the class and the
    methods in a more consistent way. This is done by moving the logic to
    resolve paramters into the private method resolvePathParameters (which
    originally only handled class paramters)
    
    The pattern now is applied for CreationFactory, queryCapability, and
    Dialogs.
    
    Also added a basic unit test for the ServiceProviderFactory class.
    
    Note: Unlike CreationFactory & QueryCapability, The URI Dialogs seem to
    be independent of the class path-annotation. Only the method
    @path-annotation is relevant. This code difference is maintained in this
    patch.
    
    Change-Id: Iaadb3c4a63b2dd344ca410a8b49f6ef49cfa7251
    Signed-off-by: Jad <jad@kth.se>
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ org.eclipse.lyo.oslc4j.core/src/test/java/org/eclipse/lyo/oslc4j/core/model/ServiceProviderFactoryTest.java
@@ -0,0 +1,68 @@
+package org.eclipse.lyo.oslc4j.core.model;
+
+import java.util.HashMap;
+import javax.ws.rs.Path;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcCreationFactory;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created on 2017-26-05
+ * @author Andrew Berezovskyi (andriib@kth.se)
+ * @version $version-stub$
+ * @since 0.0.1
+ */
+public class ServiceProviderFactoryTest {
+
+    static final String FAKE_SERVICE = "fakeService";
+    private static final String FAKE_CREATION_FACTORY = "fakeCreationFactory";
+    private static final String FAKE_CREATION_FACTORY_WITH_ID = "fakeCreationFactory/{id}";
+    private static final String ID_VALUE = "23";
+    private static final String FAKE_BASE = "http://fake.base";
+
+    @Test
+    public void createCreationFactory() throws Exception {
+        HashMap<String, Object> pathParameterValues = new HashMap<>();
+        CreationFactory creationFactory = ServiceProviderFactory.createCreationFactory(FAKE_BASE,
+                pathParameterValues, FakeService.class.getMethod("fakeCreationFactory"));
+
+        assertEquals(creationFactory.getCreation().toString(),
+                getCreationFactoryUri(FAKE_CREATION_FACTORY));
+    }
+
+    @Test
+    public void createCreationFactoryOnParametrisedMethod() throws Exception {
+        HashMap<String, Object> pathParameterValues = new HashMap<>();
+        pathParameterValues.put("id", ID_VALUE);
+        CreationFactory creationFactory = ServiceProviderFactory.createCreationFactory(FAKE_BASE,
+                pathParameterValues, FakeService.class.getMethod("fakeCreationFactoryWithId"));
+
+        String substitutedURI = FAKE_CREATION_FACTORY_WITH_ID.replace("{id}", ID_VALUE);
+        assertEquals(creationFactory.getCreation().toString(),
+                getCreationFactoryUri(substitutedURI));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createCreationFactoryOnParametrisedMethodWithNullParam() throws Exception {
+        HashMap<String, Object> pathParameterValues = new HashMap<>();
+        pathParameterValues.put("id", null);
+        ServiceProviderFactory.createCreationFactory(FAKE_BASE, pathParameterValues,
+                FakeService.class.getMethod("fakeCreationFactoryWithId"));
+    }
+
+    private String getCreationFactoryUri(final String fakeCreationFactory) {
+        return FAKE_BASE + "/" + FAKE_SERVICE + "/" + fakeCreationFactory;
+    }
+
+    @Path(FAKE_SERVICE)
+    class FakeService {
+        @OslcCreationFactory(label = "fake creation factory", title = "fake")
+        @Path(FAKE_CREATION_FACTORY)
+        public void fakeCreationFactory() {}
+
+        @OslcCreationFactory(label = "fake creation factory", title = "fake")
+        @Path(FAKE_CREATION_FACTORY_WITH_ID)
+        public void fakeCreationFactoryWithId() {}
+    }
+
+}
