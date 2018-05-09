b1fe8a4edea2b7e732f82bee15bfa4eb0005e052
Ricardo J Herrera <neormx@gmail.com>
2018-05-09 10:29:43 -0500

    Bug 533213 - rdf:Seq deserialization in JMH contains no elements
    Change-Id: Ieaa59912148e26ae943794f2201212f269b90474
    Signed-off-by: Ricardo J Herrera <neormx@gmail.com>
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ org.eclipse.lyo.oslc4j.provider.jena/src/test/java/org/eclipse/lyo/oslc4j/provider/jena/helpers/JenaAssert.java
@@ -0,0 +1,34 @@
+package org.eclipse.lyo.oslc4j.provider.jena.helpers;
+
+import org.apache.jena.rdf.model.Model;
+import org.assertj.core.api.AbstractAssert;
+
+/**
+ * Created on 2018-05-15
+ *
+ * @author Andrew Berezovskyi (andriib@kth.se)
+ * @version $version-stub$
+ * @since 0.0.1
+ */
+public class JenaAssert extends AbstractAssert<JenaAssert, Model> {
+    public JenaAssert(final Model model) {
+        super(model, JenaAssert.class);
+    }
+
+    public static JenaAssert assertThat(Model m) {
+        return new JenaAssert(m);
+    }
+
+    public JenaAssert isomorphicWith(Model m) {
+        isNotNull();
+
+        if(!actual.isIsomorphicWith(m)) {
+             failWithMessage("Expected models to be isomorphic, but they differ too much"
+                                     + ".\n\nEXPECTED:\n\n%s\n***\n\nACTUAL:\n\n%s\n===",
+                             RDFHelper.toTurtleString(m), RDFHelper.toTurtleString(actual));
+        }
+        
+        return this;
+    }
+
+}
